8bf5d51a9321ae7f00fff40a30e00874de1d1a40
jiyoon591 <jiyoon591@gmail.com>
2026-03-27 03:55:54 +0000

    Configure Firebase project credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 export const FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyBTnktADozsNujxwvvWRpamLC6TalpC58Y",
+  authDomain: "productbuilder-week1-514f2.firebaseapp.com",
+  projectId: "productbuilder-week1-514f2",
+  storageBucket: "productbuilder-week1-514f2.firebasestorage.app",
+  messagingSenderId: "668562707068",
+  appId: "1:668562707068:web:cf51e71f65f9e792f5eff9",
 };
